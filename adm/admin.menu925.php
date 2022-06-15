43930a9cb0047ddf6c931bfdcbd03915066d179c
hanjoo <hanjoo@epcs.co.kr>
2022-06-15 02:24:51 +0000

    James 0615 ap1124

--- adm/admin.menu925.php
+++ adm/admin.menu925.php
@@ -1,9 +1,9 @@
 <?php
 $menu["menu925"] = array (
-    array('925000', '통합설비관리', ''.G5_USER_ADMIN_URL.'/cubic/mes_charge_in.php', 'mes_charge_in'),
-    array('925110', '실시간모니터링', ''.G5_USER_ADMIN_URL.'/cubic/mes_charge_in.php', 'mes_charge_in'),
-    array('925120', '설비측정그래프', ''.G5_USER_ADMIN_URL.'/cubic/mes_charge_out.php', 'mes_charge_out'),
-    array('925140', '인공지능', ''.G5_USER_ADMIN_URL.'/cubic/mes_cast_shot.php', 'mes_cast_shot'),
-    array('925900', '설비관리설정', ''.G5_USER_ADMIN_URL.'/config_form.php', 'config_form')
+    array('925000', '통합설비관리', ''.G5_USER_ADMIN_URL.'/system/index.php', 'index'),
+    array('925110', '실시간모니터링', ''.G5_USER_ADMIN_URL.'/system/index.php', 'index'),
+    array('925120', '설비측정그래프', ''.G5_USER_ADMIN_URL.'/system/index.php', 'index'),
+    array('925140', '인공지능', ''.G5_USER_ADMIN_URL.'/system/index.php', 'index'),
+    array('925900', '설비관리설정', ''.G5_USER_ADMIN_URL.'/system/config_form.php', 'config_form')
 );
 ?>
