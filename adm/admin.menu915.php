f8cc28959ee528d149d3b76db6b8054d9182d380
hanjoo <hanjoo@epcs.co.kr>
2022-06-28 04:34:34 +0000

    James 0628 pm0134

--- adm/admin.menu915.php
+++ adm/admin.menu915.php
@@ -3,6 +3,7 @@ $menu["menu915"] = array (
     array('915000', '대시보드', ''.G5_USER_ADMIN_URL.'/index.php', 'index'),
     array('915110', '대시보드', ''.G5_USER_ADMIN_URL.'/index.php', 'index'),
     array('915120', '제1대시보드', ''.G5_USER_ADMIN_URL.'/index1.php', 'index1'),
-    array('915130', '제2대시보드', ''.G5_USER_ADMIN_URL.'/index2.php', 'index2')
+    array('915130', '제2대시보드', ''.G5_USER_ADMIN_URL.'/index2.php', 'index2'),
+    array('915900', '대시보드설정', ''.G5_USER_ADMIN_URL.'/config_dashboard_form.php', 'config_dashboard_form')
 );
 ?>
